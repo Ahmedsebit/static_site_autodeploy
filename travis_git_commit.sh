d3e748401ed076f762af81c64d367c8aad20a243
GathuBoswell <gathudev@gmail.com>
2017-07-21 13:34:25 +0300

    Refactore for proper method to allow Travis to commit to Github

--- travis_git_commit.sh
+++ travis_git_commit.sh
@@ -2,12 +2,14 @@
 
 setup_git() {
   cd ../
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "boswell.gathu@andela.com"
+  git config --global user.name "boswellgathu"
 }
 
 commit_website_files() {
   git checkout $DEPLOY_BRANCH
+  git remote rm origin
+  git remote add origin https://boswellgathu:$GITHUB_API_KEY@github.com/boswellgathu/static_site_autodeploy.git
   git add $INDEX_FILE
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
